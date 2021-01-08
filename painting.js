ae7a295b1862741a03cc484e0368a5e471cb7bac
kanchanamala85 <76962064+kanchanamala85@users.noreply.github.com>
2021-01-08 20:48:29 +0530

    Alter Asset Rendering
    Change 3 column layout to four column layout.

--- painting.js
+++ painting.js
@@ -5,8 +5,10 @@ var c3=document.getElementById('c3');
 var c4=document.getElementById('c4');
 var c5=document.getElementById('c5');
 var c6=document.getElementById('c6');
-var col_array_portrait=[c1,c2,c3];
-var col_array_landscape=[c4,c5,c6];
+var c7=document.getElementById('c7');
+var c8=document.getElementById('c8');
+var col_array_portrait=[c1,c2,c3,c4];
+var col_array_landscape=[c5,c6,c7,c8];
 var path="assets/paintings/painting";
 var c=0;
 var p=['1','2','4','5','6','8','9','12','14','15'];
@@ -16,7 +18,7 @@ for(i=0;i<p.length;i++){
     im.setAttribute("class","materialboxed portrait");
     //im.setAttribute("class","");
     im.setAttribute("src",path+p[i]+'.jpg');
-    col_array_portrait[c%3].appendChild(im)
+    col_array_portrait[c%4].appendChild(im)
     c+=1;
 }
 c=0;
@@ -25,6 +27,6 @@ for(i=0;i<l.length;i++){
     im.setAttribute("class","materialboxed landscape");
     //im.setAttribute("class","");
     im.setAttribute("src",path+l[i]+'.jpg');
-    col_array_landscape[c%3].appendChild(im)
+    col_array_landscape[c%4].appendChild(im)
     c+=1;
 }
